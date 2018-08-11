1e686234da77ab991de7a7457cafde43cccda02b
Prapojjanaporn, Pruet (Agoda) <pprapojjanap@c02th23yfvh4.agoda.local>
2018-08-11 11:57:22 +0700

    Initial commit

---
+++ src/main/java/com/example/SpringBootExample/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.SpringBootExample.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @RequestMapping("/")
+    public String home(Model model){
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
